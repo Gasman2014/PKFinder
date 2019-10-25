90e87d7a9b9a011674aa5131362601f13b7424de
John <johnpateman@me.com>
2019-10-25 15:24:24 +0100

    Added Axial & Radial i.e. THT style components
    Now search for THT components too.
    e.g. R_Axial_10K, C_Radial_10u

--- part.py
+++ part.py
@@ -1,12 +1,31 @@
-#coding=utf-8
-from mysql.connector import MySQLConnection, Error
-from python_mysql_dbconfig import read_db_config
-import sys
+#!/usr/bin/env python3
+# coding=utf-8
+
 import decimal
+import sys
+from engineering_notation import EngNumber
+
+sys.path.insert(0,"/usr/local/bin/python_mysql_dbconfig")
+from mysql.connector import Error, MySQLConnection
+from python_mysql_dbconfig import read_db_config
 
 ctx = decimal.Context()
 ctx.prec = 12
 
+ref = sys.argv[1]
+# TODO Add -h option
+
+# TODO Messy quick hack to deal with THT
+# All SMD case styles were covered by a 4 digit code which could easily be parsed
+# by splitting off 6 caracters. However, have added 'Axial and 'Radial' case styles for THT
+# This needs to be handled rather differently to be more robust solution.
+# Options Simply use leading 'R_ or C_' to trigger parsing into words - risk if normal component begins C_ ...
+# Use a dialogue - enter R > prompt ?case style ?Value - don't like this as it is too long winded
+
+#resistors = ["R_1206", "R_0805", "R_0603", "R_0402", "R_AXIA", "R_Axia"]
+#capacitors = ["C_1206", "C_0805", "C_0603", "C_0402", "C_RADI", "C_Radi", "C_AXIA", "C_Axia"]
+quality = ""
+variant = ""
 
 def float_to_str(f):
     d1 = ctx.create_decimal(repr(f))
@@ -15,7 +34,8 @@ def float_to_str(f):
 
 def convert_units(num):
     factors = ["G", "M", "K", "k", "R", "", ".", "m", "u", "n", "p"]
-    conversion = {'G': '1000000000', 'M': '1000000', 'K': '1000', 'k': '1000', 'R': '1', '.': '1', '': '1', 'm': '0.001', "u": '0.000001', 'n': '0.000000001', 'p': '0.000000000001'}
+    conversion = {'G': '1000000000', 'M': '1000000', 'K': '1000', 'k': '1000', 'R': '1',
+                  '.': '1', '': '1', 'm': '0.001', "u": '0.000001', 'n': '0.000000001', 'p': '0.000000000001'}
     val = ""
     mult = ""
 
@@ -43,28 +63,54 @@ def convert_units(num):
     return(r)
 
 
-ref = sys.argv[1]
+def partStatus(partID, parameter):
+    dbconfig = read_db_config()
+    try:
+        conn = MySQLConnection(**dbconfig)
+        cursor = conn.cursor()
+        sql = "SELECT R.stringValue FROM PartParameter R WHERE (R.name = '{}') AND (R.part_id = {})".format(
+            parameter, partID)
+        cursor.execute(sql)
+        partStatus = cursor.fetchall()
 
-resistors = ["R_1206", "R_0805", "R_0603", "R_0402"]
-capacitors = ["C_1206", "C_0805", "C_0603", "C_0402"]
-quality = ""
-variant = ""
+        if partStatus == []:
+            part = "Unknown"
+        else:
+            part = str(partStatus[0])[2:-3]
+        return (part)
 
+    except UnicodeEncodeError as err:
+        print(err)
+
+    finally:
+        cursor.close()
+        conn.close()
 
 
 def find_part(part_num):
 
+    # Initialise database, set for p/n searching unless a 'bean' and init optional parameters
     dbconfig = read_db_config()
     bean = False
+    c_characteristics = ""
+    c_value = ""
+    c_case = ""
+    value = ""
+
     try:
         conn = MySQLConnection(**dbconfig)
         cursor = conn.cursor()
 
-        if (part_num[:6]) in resistors:
+        #This was OK when 2nd parameter always had 4 digits
+        # Might be better something like 'if (part_num)[:2] = "R_" ... or C_ "
+
+        #  if (part_num[:6]) in resistors:
+        if (part_num[:2]) == "R_":
             quality = "Resistance"
             variant = "Resistance Tolerance"
             bean = True
-        if (part_num[:6]) in capacitors:
+        #if (part_num[:6]) in capacitors:
+        if (part_num[:2]) == "C_":
             quality = "Capacitance"
             variant = "Dielectric Characteristic"
             bean = True
@@ -73,16 +119,17 @@ def find_part(part_num):
             component = part_num.split('_')
 
             if (len(component)) <= 2:
-                print ("Insufficient parameters (Needs 3 or 4) e.g. R_0805_100K_±5%")
+                print("Insufficient parameters (Needs 3 or 4) e.g. R_0805_100K_±5%")
                 return ("0")
 
             c_case = component[1]
             c_value = convert_units(component[2])
+
             if (len(component)) == 4:
                 c_characteristics = component[3]
 
-                #A fully specified bean
-                sql = """SELECT P.name, P.description, P.stockLevel, P.internalPartNumber, S.name
+                # A fully specified bean
+                sql = """SELECT P.name, P.description, P.stockLevel, P.internalPartNumber, S.name, P.id
                             FROM Part P
                             JOIN PartParameter R ON R.part_id = P.id
                             JOIN StorageLocation S ON  S.id = P.storageLocation_id
@@ -95,8 +142,8 @@ def find_part(part_num):
                             COUNT(DISTINCT R.name)=3""".format(c_case, quality, c_value, variant, c_characteristics)
             else:
 
-                #A partially specified bean
-                sql = """SELECT P.name, P.description, P.stockLevel, P.internalPartNumber, S.name
+                # A partially specified bean
+                sql = """SELECT P.name, P.description, P.stockLevel, P.internalPartNumber, S.name,P.id
                             FROM Part P
                             JOIN PartParameter R ON R.part_id = P.id
                             JOIN StorageLocation S ON  S.id = P.storageLocation_id
@@ -107,8 +154,9 @@ def find_part(part_num):
                             HAVING
                             COUNT(DISTINCT R.name)=2""".format(c_case, quality, c_value)
         else:
-            #A specific part number
-            sql = """SELECT P.name, P.description, P.stockLevel, P.internalPartNumber, S.name
+
+            # A specific part number
+            sql = """SELECT P.name, P.description, P.stockLevel, P.internalPartNumber, S.name, P.id
                     FROM Part P
                     JOIN StorageLocation S ON  S.id = P.storageLocation_id
                     WHERE P.name LIKE '%{}%'""".format(part_num)
@@ -116,12 +164,16 @@ def find_part(part_num):
         cursor.execute(sql)
         components = cursor.fetchall()
         n_components = len(components)
-        print ("{} component(s) meet the search pattern".format(n_components))
-
+        print("{} component(s) meet the search pattern".format(n_components))
+        if c_value != "":
+            value = EngNumber(c_value)
         if components == []:
             print("No components match this pattern")
         else:
-            for (name, description, stockLevel, partNum, storageID) in components:
+            for (name, description, stockLevel, partNum, storageID, PKid) in components:
+                ROHS = partStatus(PKid, 'RoHS')
+                Lifecycle = partStatus(PKid, 'Lifecycle Status')
+
                 print()
                 print((' {:=<102}').format(""))
                 print((" | {:17}| {:80.80}|").format(name, description))
@@ -129,6 +181,12 @@ def find_part(part_num):
                 print((" | Part ID          | {:80}|").format(partNum))
                 print((" | Storage location | {:80}|").format(storageID))
                 print((" | {:17}| {:<80}|").format("Stock", stockLevel))
+                print((" | {:17}| {:<80}|").format("Status", Lifecycle))
+                print((" | {:17}| {:<80}|").format("RoHS", ROHS))
+                print((" | {:17}| {:<80}|").format("Package", c_case))
+                print((" | {:17}| {:<80}|").format("Value", str(value)))
+                if c_characteristics != "":
+                    print((" | {:17}| {:<80}|").format("Variant", c_characteristics))
                 print((' {:=<102}').format(""))
                 print()
 
